4db25da8fa28fc410cc5c9244c20c636edbd953e
prikhodkin <info@prikhodkin.com>
2020-10-15 09:05:04 +0500

    Добавляет шапку

--- src/js/general.js
+++ src/js/general.js
@@ -1,11 +1,8 @@
 import Turbolinks from "turbolinks";
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
-
 import Tabs from "%modules%/tabs/tabs"
-import {$, $$} from "./util";
-
-
+import $ from "jquery";
 Turbolinks.start();
 
 const application = Application.start()
@@ -13,5 +10,18 @@ const context = require.context("./controllers", true, /\.js$/);
 application.load(definitionsFromContext(context));
 
 
-const tabs = $(`[data-tabs]`);
-new Tabs(tabs);
+$(`.burger`).on(`click`, (evt) => {
+  evt.preventDefault();
+  $(`.burger__inner`).toggleClass(`burger__inner--active`);
+  $(`.header__inner`).toggleClass(`header__inner--active`);
+})
+
+
+const subItems = document.querySelectorAll(`.navigation__sub-item`);
+const dropdownItems = document.querySelectorAll(`.navigation__dropdown-item`);
+const changeWidth = (item) => {
+  item.style.width = item.offsetWidth + 3 + 'px';
+}
+
+subItems.forEach((item) => changeWidth(item));
+dropdownItems.forEach((item) => changeWidth(item));
